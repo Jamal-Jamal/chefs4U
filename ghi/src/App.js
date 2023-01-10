2073c451b6e781815f8e649b7ef53c9446d24043
kayre <kayresantos@gmail.com>
2023-01-09 16:22:09 -0800

    fixing failed merge

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -1,16 +1,17 @@
-import React, { useState} from 'react';
+// import React, { useState} from 'react';
 import './App.css';
-import SignUpForm from "./SignUpForm"
-import LoginForm from "./LoginForm"
+// import SignUpForm from "./SignUpForm"
+// import LoginForm from "./LoginForm"
 
 function App() {
-    const [currentForm, setCurrentForm] = useState('login');
+    // const [currentForm, setCurrentForm] = useState('login');
 
     return (
         <div className = "App">
-            {
+            <p>"Hello"</p>
+            {/* {
                 currentForm === "login" ? <LoginForm /> : <SignUpForm />
-            }
+            } */}
         </div>
 
     );
--- ghi/src/LoginForm.js
+++ ghi/src/LoginForm.js
@@ -1,26 +1,27 @@
-import React, {useState} from 'react';
+// import React, {useState} from 'react';
 
 
-export const LoginForm = () => {
-    const [username, setUsername] = useState('');
-    const[pass, setPass] = useState('');
+// export const LoginForm = () => {
+//     const [username, setUsername] = useState('');
+//     const[pass, setPass] = useState('');
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-    }
+//     const handleSubmit = (e) => {
+//         e.preventDefault();
+//     }
 
-    return (
-        <>
-            <form onSubmit={handleSubmit}>
-                <label for="username">username</label>
-                <input value={username} type="username" placeholder="username" id="username" name="username" />
-                <label for="password">password</label>
-                <input value={pass} type="password" placeholder="*******" id="password" name="password" />
-                <button type="submit">Login</button>
-            </form>
-            <button>Don't have an account? Sign up here</button>
-        </>
-    )
-}
+//     return (
+//         <>
+//             <form onSubmit={handleSubmit}>
+//                 <label for="username">username</label>
+//                 <input value={username} onChange={e => setUsername(e.target.value)}type="username" placeholder="username" id="username" name="username" />
+//                 <label for="password">password</label>
+//                 <input value={pass} onChange={e => setPass(e.target.value)}type="password" placeholder="*******" id="password" name="password" />
+//                 <button type="submit">Login</button>
+//             </form>
+//             <button>Don't have an account? Sign up here</button>
+//         </>
+//     )
 
-export default LoginForm
+// }
+
+// export default LoginForm
