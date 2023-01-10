f83dfda582b45b367694474267c19123afff067d
Kayre Santos <kayresantos@gmail.com>
2023-01-10 00:37:31 +0000

    created account/user table, chef attributes are optional, and added POST endpoint...

---
+++ ghi/src/App.js
@@ -0,0 +1,20 @@
+// import React, { useState} from 'react';
+import './App.css';
+// import SignUpForm from "./SignUpForm"
+// import LoginForm from "./LoginForm"
+
+function App() {
+    // const [currentForm, setCurrentForm] = useState('login');
+
+    return (
+        <div className = "App">
+            <p>"Hello"</p>
+            {/* {
+                currentForm === "login" ? <LoginForm /> : <SignUpForm />
+            } */}
+        </div>
+
+    );
+}
+
+export default App;
