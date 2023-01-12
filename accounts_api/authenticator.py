686044bd6ef61db51565922c20117977bd45664e
kayre <kayresantos@gmail.com>
2023-01-11 23:07:27 -0800

    merge branch to do login form token

--- accounts_api/authenticator.py
+++ accounts_api/authenticator.py
@@ -2,29 +2,6 @@
 import os
 from fastapi import Depends
 from jwtdown_fastapi.authentication import Authenticator
-<<<<<<< HEAD
-from queries.accounts_queries import (
-    AccountRepository,
-    AccountOut,
-    AccountOutWithPassword
-)
-
-
-class AppAuthenticator(Authenticator):
-    async def get_account_data(
-        self,
-        username: str,
-        accounts: AccountRepository,
-    ):
-        # Use your repo to get the account based on the
-        # username (which could be an email)
-        repo = AccountRepository()
-        return repo.get_user(username)
-
-    def get_account_getter(
-        self,
-        accounts: AccountRepository = Depends()) -> AccountRepository:
-=======
 from queries.accounts_queries import AccountRespository, AccountOut, AccountOutWithPassword
 
 
@@ -42,19 +19,13 @@ class MyAuthenticator(Authenticator):
         self,
         accounts: AccountRespository = Depends(),
     ):
->>>>>>> testing-backend-auth
         # Return the accounts. That's it.
         return accounts
 
     def get_hashed_password(self, account: AccountOutWithPassword):
         # Return the encrypted password value from your
         # account object
-<<<<<<< HEAD
-        print("***HASHED PASSWORD***", account)
-        return account.hashed_password
-=======
         return account.password
->>>>>>> testing-backend-auth
 
     def get_account_data_for_cookie(self, account: AccountOut):
         # Return the username and the data for the cookie.
@@ -62,8 +33,4 @@ class MyAuthenticator(Authenticator):
         return account.username, AccountOut(**account.dict())
 
 
-<<<<<<< HEAD
-authenticator = AppAuthenticator(os.environ["SIGNING_KEY"])
-=======
 authenticator = MyAuthenticator(os.environ["SIGNING_KEY"])
->>>>>>> testing-backend-auth
