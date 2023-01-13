0398ccc848e75af3f378ee8906517ca0c9edf462
Ryan <ryanlu294@gmail.com>
2023-01-13 10:21:24 -0800

    fixes cors issue from ghi to accounts

--- accounts_api/main.py
+++ accounts_api/main.py
@@ -1,8 +1,21 @@
-from authenticator import authenticator
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import os
 from routers import accounts_routers
+from authenticator import authenticator
 
 
 app = FastAPI()
 app.include_router(authenticator.router)
 app.include_router(accounts_routers.router)
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        os.environ.get("CORS_HOST", "http://localhost:3000")
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
