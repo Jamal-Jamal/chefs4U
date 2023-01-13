0586d0a6a78a97ce823de7b2b8eddbacd200c31c
Ryan <ryanlu294@gmail.com>
2023-01-13 15:32:32 -0800

    fixes import again

--- accounts_api/main.py
+++ accounts_api/main.py
@@ -1,9 +1,8 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import os
-import authenticator
 from routers import accounts_routers
-
+from authenticator import authenticator
 
 app = FastAPI()
 app.include_router(authenticator.router)
