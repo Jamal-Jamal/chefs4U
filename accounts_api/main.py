773f6905dfea4faf47b48d5db74ed7197efbe46f
Ryan <ryanlu294@gmail.com>
2023-01-21 00:08:16 -0800

    updates ci and cors

--- accounts_api/main.py
+++ accounts_api/main.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-import os
 from routers import accounts_routers
 from authenticator import authenticator
 
@@ -8,11 +7,14 @@ app = FastAPI()
 app.include_router(authenticator.router)
 app.include_router(accounts_routers.router)
 
+origins = [
+    "http://localhost:3000",
+    "https://chefs4u.gitlab.io",
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        os.environ.get("CORS_HOST", "http://localhost:3000")
-    ],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
--- events_api/main.py
+++ events_api/main.py
@@ -1,17 +1,18 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-import os
 from routers import events_routers
 
 app = FastAPI()
 app.include_router(events_routers.router)
 
+origins = [
+    "http://localhost:3000",
+    "https://chefs4u.gitlab.io",
+]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        os.environ.get("CORS_HOST", "http://localhost:3000")
-    ],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
