f1424dfdaf3d7c700becf7ffbbfaa23ee05ffdc2
Alissa Ramos <alipdx25@gmail.com>
2023-01-26 01:47:14 +0000

    Favorite event button

--- ghi/src/Accounts/Authentication.js
+++ ghi/src/Accounts/Authentication.js
@@ -77,7 +77,10 @@ export function useToken() {
   async function logout() {
     if (token) {
       const url = `${process.env.REACT_APP_ACCOUNTS_HOST}/token`;
-      await fetch(url, { method: "delete", credentials: "include" });
+      await fetch(url, {
+        method: "delete",
+        credentials: "include",
+      });
       internalToken = null;
       setToken(null);
       navigate("/");
